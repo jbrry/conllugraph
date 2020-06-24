244e8ba48c779eb52a0a9f3e524c2c95871bee5b
jbar-ry <james.barry26@mail.dcu.ie>
2020-06-24 01:10:57 +0100

    add arg for morphological case

--- scripts/run.sh
+++ scripts/run.sh
@@ -2,9 +2,12 @@
 
 DATA_DIR=data/iwpt2020stdata
 
+# ar_padt:bg_btb:cs_cac:cs_fictree:cs_pdt:en_ewt:et_edt:fi_tdt:fr_sequoia:it_isdt:lt_alksnis:lv_lvtb:nl_alpino:nl_lassysmall:nl_lassysmall:pl_lfg:pl_pdb:ru_syntagrus:sk_snk:sv_talbanken:ta_ttb:uk_iu
+
 test -z $1 && echo "Missing list of TBIDs (space or colon-separated)"
 test -z $1 && exit 1
 TBIDS=$(echo $1 | tr ':' ' ')
+
 
 out_file=log.txt
 
@@ -15,15 +18,26 @@ else
 fi 
 
 for tbid in $TBIDS ; do
+
+  if [ "$tbid" = "ar_padt" ] || [ "$tbid" = "cs_cac" ] || [ "$tbid" = "cs_fictree" ] || [ "$tbid" = "cs_pdt" ]; then
+    extra_args="--attach_morphological_case"
+  else
+    extra_args=""
+  fi
+
   echo "" >> $out_file
   echo "==== $tbid ====" >> $out_file
   for filepath in ${DATA_DIR}/UD_*/${tbid}-ud-train.conllu; do
-  dir=`dirname $filepath`        # e.g. /home/user/ud-treebanks-v2.2/UD_Afrikaans-AfriBooms
-  tb_name=`basename $dir`        # e.g. UD_Afrikaans-AfriBooms
+  dir=`dirname ${filepath}`        # e.g. /home/user/ud-treebanks-v2.2/UD_Afrikaans-AfriBooms
+  tb_name=`basename ${dir}`        # e.g. UD_Afrikaans-AfriBooms
 
   python run.py \
-  -g $DATA_DIR/${tb_name}/${tbid}-ud-test.conllu \
-  -s $DATA_DIR/sysoutputs/adapt/test/pertreebank/$tbid-ud-test-sys.conllu \
+  -g ${DATA_DIR}/${tb_name}/${tbid}-ud-test.conllu \
+  -s ${DATA_DIR}/sysoutputs/adapt/test/pertreebank/$tbid-ud-test-sys.conllu \
+  ${extra_args} \
   >> $out_file
+
+  echo "" >> $out_file
+  echo "==========" >> $out_file
   done
 done
